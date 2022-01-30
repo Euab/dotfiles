c47141cc57935aedc45420bea42978e929dbf378
Euab <euanmills26@gmail.com>
2022-01-30 19:02:09 +0000

    Initial commit

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,12 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
+
+# Aliases
+Set-Alias vim nvim
+Set-Alias vi nvim
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
---
+++ README.md
@@ -0,0 +1,5 @@
+![cover](./images/cover.png)
+
+# Euan's Dotfiles
+
+This is a work in progress... There is nothing worth cloning right now.
